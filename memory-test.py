a8b7d41855ba09bcc5ee7213a26380e86239d767
jeremyl <jeremy.lentini2@gmail.com>
2019-11-23 16:00:24 -0500

    added comments to test files

--- memory-test.py
+++ memory-test.py
@@ -1,6 +1,9 @@
 import subprocess
 
-
+# this method attempts to measure memory used
+# it runs the free command and parses the output
+# to see how much memory is being used before and after
+# the command runs
 def estimate_memory_use(cmd):
     start = subprocess.check_output(["free"]).decode()
     start_used_memory = int(start.split("\n")[1].split("    ")[2])
@@ -9,9 +12,11 @@ def estimate_memory_use(cmd):
     stop_used_memory = int(stop.split("\n")[1].split("    ")[2])
     return stop_used_memory - start_used_memory
 
+# runs each of the commands specifies below
+# change num_runs to change the amount of times each task is called
 def run_commands(build_tool, commands):
     memory_use = []
-    num_runs = 1
+    num_runs = 5
     for cmd in commands:
         print("Running {} {} times".format(cmd, num_runs))
         mem_sum = 0
@@ -27,10 +32,8 @@ def run_commands(build_tool, commands):
     return memory_use
 
 
-# ant_commands = ["site", "compile", "compile-version", "release-notes", "jenkins", "clean", "check-ooxml-jars", "fetch-ooxml-jars", "init"]
-ant_commands = ["compile"]
-# gradle_commands = ["g-site", "g-compile", "g-compile-version", "g-release-notes", "g-jenkins", "g-clean", "g-check-ooxml-jars", "g-fetch-ooxml-jars", "g-init"]
-gradle_commands = ["g-compile"]
+ant_commands = ["site", "compile", "compile-version", "release-notes", "jenkins", "clean", "check-ooxml-jars", "fetch-ooxml-jars", "init"]
+gradle_commands = ["g-site", "g-compile", "g-compile-version", "g-release-notes", "g-jenkins", "g-clean", "g-check-ooxml-jars", "g-fetch-ooxml-jars", "g-init"]
 
 ant_mem_use = run_commands("ant", ant_commands)
 gradle_mem_use = run_commands("./gradlew", gradle_commands)
